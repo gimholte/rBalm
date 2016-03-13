53a14e6369560af0ee00f57b5df314f58dbaea59
Gregory Imholte <gimholte@gmail.com>
2016-03-13 14:23:05 -0700

    initial commit

---
+++ R/initialize.R
@@ -0,0 +1,15 @@
+#' @title Initialize data structures for rBalm algorithm
+#' @description todo
+#' @details internal
+#' @param input A data.table with bead data
+#' @param control A list with MCMC parameters
+#' 
+#' 
+#' 
+#' @return List with required rBalm structures 
+#' @export 
+
+
+rBalmInit <- function(input, control) {
+    return(NULL)
+}
---
+++ R/rBalm.R
@@ -0,0 +1,14 @@
+#' @title Robust Bayesian analysis of Luminex bead-based assays
+#' @description todo description
+#' @details todo details
+#' @param data todo
+#' @param n_iter todo 
+#' @param n_burn todo
+#' 
+#' @return some output
+#' @export 
+
+rBalm <- function(data, n_iter, n_burn) {
+    d_list <- list(x = rnorm(100))
+    .Call("rBalmMcmc", d_list, PACKAGE = "rBalm")
+}
