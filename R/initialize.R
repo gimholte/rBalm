97f5044b871bdf3747b749f5d1286e2f80489d14
Gregory Imholte <gimholte@gmail.com>
2016-03-13 22:26:32 -0700

    Add functions to initialize bead distributions

--- R/initialize.R
+++ R/initialize.R
@@ -13,3 +13,43 @@
 rBalmInit <- function(input, control) {
     return(NULL)
 }
+
+#' @title Initialize bead list element for MCMC
+#'  
+#' @description Given a list of bead distributions, process each list element
+#' for efficient evaluation of the Laplace likelihood 
+#' 
+#' @details doto
+#' 
+#' @param bead_dists List of atomic double vectors containing bead measurements
+#' @return List of bead distributions, in the same order, with summary information
+#' 
+#' @export 
+
+beadListInit <- function(bead_dists) {
+    out <- lapply(bead_dists, processBeadVector)
+    return(out)
+}
+
+#' @title Process a bead distribution
+#' 
+#' @description Summary statistics and various quantities 
+#' needed for Laplace density evaluation
+#' 
+#' @param x bead values
+#' @details The list is named 
+#' 
+#' @return Return a name list with elements sort_x, med, j, d, sum_d
+#' @export
+
+processBeadVector <- function(x) {
+    sort_x <- sort(x)    
+    med <- median(sort_x)
+    j <- max(which(sort_x <= med))
+    d <- abs(sort_x - med)
+    sum_d <- sum(d)
+
+    out <- list(sort_x, med, j, d, sum_d)
+    names(out) <- c("sort_x", "median", "j", "d", "sum_d")
+    return(out)
+}
