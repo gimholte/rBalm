07c189d4bf61ac7eeed832a85ac10077912a6006
Gregory Imholte <gimholte@gmail.com>
2016-03-13 22:27:28 -0700

    Add test for evaluating sum of absolute deviations

--- src/rbalm.cpp
+++ src/rbalm.cpp
@@ -11,6 +11,11 @@ RcppExport SEXP rBalmMcmc(SEXP data_input) {
     return data_list;
 }
 
+/*
+ * Evaluate the sum of absolute deviations a list of bead distributions
+ * x_bead_list: R list of processed bead distributions
+ * x_eval: R numeric vector of values at which to evaluate the SADs
+ */
 RcppExport SEXP beadListTest(SEXP x_bead_list, SEXP x_eval) {
     List bead_list(x_bead_list);
     NumericVector eval(x_eval);
