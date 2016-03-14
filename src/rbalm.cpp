28049b116459b8773b36ccedd7a03635c534bdb4
Gregory Imholte <gimholte@gmail.com>
2016-03-14 14:34:17 -0700

    create MCMC test method and streamline bead list parsing

--- src/rbalm.cpp
+++ src/rbalm.cpp
@@ -2,7 +2,7 @@
 #include "Rcpp.h"
 #include "RngStream.h"
 #include "Rv.h"
-#include "beadList.h"
+#include "beadDist.h"
 #include <vector>
 using namespace Rcpp;
 
@@ -18,15 +18,11 @@ RcppExport SEXP rBalmMcmc(SEXP data_input) {
  */
 RcppExport SEXP beadListTest(SEXP x_bead_list, SEXP x_eval) {
     List bead_list(x_bead_list);
+    List bead_prior = List::create(_["prior_shape"] = 1.0, _["prior_rate"] = 1.0);
     NumericVector eval(x_eval);
     NumericVector out(eval.size());
-    std::vector<beadList> blist;
-
-    List::iterator in_iter;
-    for (in_iter = bead_list.begin(); in_iter != bead_list.end(); ++in_iter) {
-        List ll(*in_iter);
-        blist.push_back(beadList(ll, 1.0, 1.0));
-    }
+    std::vector<beadDist> blist;
+    parseBeadDistList(bead_list, blist, bead_prior);
 
     if (eval.size() != bead_list.size())
         return wrap(R_NilValue);
@@ -38,4 +34,30 @@ RcppExport SEXP beadListTest(SEXP x_bead_list, SEXP x_eval) {
     return wrap(out);
 }
 
+RcppExport SEXP beadListMcmcTest(SEXP r_bead_list, SEXP r_chain_ctl,
+        SEXP r_bead_prior_pars) {
+    RngStream rng = RngStream_CreateStream("");
+    List bead_list(r_bead_list);
+    List chain_ctl(r_chain_ctl);
+    List bead_prior_pars(r_bead_prior_pars);
 
+    int n_iter = as<int>(chain_ctl["n_iter"]);
+    int n_bead_dist = bead_list.size();
+
+    Dimension out_dim(n_bead_dist, n_iter);
+    NumericMatrix out_mu(out_dim);
+    NumericMatrix out_prec(out_dim);
+
+    std::vector<beadDist> bead_distributions;
+    parseBeadDistList(bead_list, bead_distributions, bead_prior_pars);
+
+    for(int i = 0; i < n_iter; i++) {
+        for (int j = 0; j < n_bead_dist; j++) {
+            bead_distributions[j].updateLaplaceMu(rng, 1.0, 1.0);
+            bead_distributions[j].updateLaplacePrecision(rng);
+            out_mu(j, i) = bead_distributions[j].getMu();
+            out_prec(j, i) = bead_distributions[j].getPrecision();
+        }
+    }
+    return wrap(List::create(_["mu"] = out_mu, _["prec"] = out_prec));
+}
--- tests/testthat/test_beadstuff.R
+++ tests/testthat/test_beadstuff.R
@@ -1,45 +1,68 @@
 library(rBalm)
 
-set.seed(123)
-n <- c(50, 51, 100, 200, 505)
-beads <- lapply(n, rnorm, mean = 0, sd = 1)
-processed_beads <- beadListInit(beads)
-
 test_that("bead distributions have proper named attributes", {
+            set.seed(123)
+            n <- c(50, 51, 100, 200, 505)
+            beads <- lapply(n, rnorm, mean = 0, sd = 1)
+            processed_beads <- beadListInit(beads)
+            
             expect_true(all(names(processed_beads[[1]]) %in%
                                     c("sort_x", "sum_d", "d", "j", "median")))
         })
 
-beadListTest <- function(beads, at) {
-    out <- .Call("beadListTest", beads, at, PACKAGE = "rBalm")
-    return(out)
-}
-
-eval_at <- c(-100, .5, 0, .5, 100)
-sum_abs_devs <- mapply(function(vec, eval) sum(abs(vec - eval)), beads, eval_at)
-sum_abs_devs_cpp <- beadListTest(processed_beads, eval_at)
-
 test_that("sum of absolute deviations are correct", {
-    expect_equal(sum_abs_devs_cpp[1], sum_abs_devs[1])
-    expect_equal(sum_abs_devs_cpp[2], sum_abs_devs[2])
-    expect_equal(sum_abs_devs_cpp[3], sum_abs_devs[3])
-    expect_equal(sum_abs_devs_cpp[4], sum_abs_devs[4])
-    expect_equal(sum_abs_devs_cpp[5], sum_abs_devs[5])
-})
-
-
-beads <- lapply(n, rpois, lambda = 2)
-processed_beads <- beadListInit(beads)
-
-eval_at <- c(-1, 0, 1, 3, 9)
-sum_abs_devs <- mapply(function(vec, eval) sum(abs(vec - eval)), beads, eval_at)
-sum_abs_devs_cpp <- beadListTest(processed_beads, eval_at)
+            set.seed(123)
+            n <- c(50, 51, 100, 200, 505)
+            beads <- lapply(n, rnorm, mean = 0, sd = 1)
+            processed_beads <- beadListInit(beads)
+            
+            beadListTest <- function(beads, at) {
+                out <- .Call("beadListTest", beads, at, PACKAGE = "rBalm")
+                return(out)
+            }
+            
+            eval_at <- c(-100, .5, 0, .5, 100)
+            sum_abs_devs <- mapply(function(vec, eval) sum(abs(vec - eval)), beads, eval_at)
+            sum_abs_devs_cpp <- beadListTest(processed_beads, eval_at)
+            expect_equivalent(sum_abs_devs_cpp, sum_abs_devs)
+        })
 
 test_that("sum of absolute deviations are correct (with ties)", {
-            expect_equal(sum_abs_devs_cpp[1], sum_abs_devs[1])
-            expect_equal(sum_abs_devs_cpp[2], sum_abs_devs[2])
-            expect_equal(sum_abs_devs_cpp[3], sum_abs_devs[3])
-            expect_equal(sum_abs_devs_cpp[4], sum_abs_devs[4])
-            expect_equal(sum_abs_devs_cpp[5], sum_abs_devs[5])
+            set.seed(123)
+            n <- c(50, 51, 100, 200, 505)
+            beads <- lapply(n, rpois, lambda = 2)
+            processed_beads <- beadListInit(beads)
+            beadListTest <- function(beads, at) {
+                out <- .Call("beadListTest", beads, at, PACKAGE = "rBalm")
+                return(out)
+            }
+            
+            eval_at <- c(-1, 0, 1, 3, 9)
+            sum_abs_devs <- mapply(function(vec, eval) sum(abs(vec - eval)), beads, eval_at)
+            sum_abs_devs_cpp <- beadListTest(processed_beads, eval_at)
+            expect_equivalent(sum_abs_devs_cpp, sum_abs_devs)
+        })
+
+
+test_that("markov chain monte carlo successfully executes", {
+            set.seed(123)
+            beadListMcmcTest <- function(beads, n_iter) {
+                processed_beads <- beadListInit(beads);
+                bead_prior <- list("prior_shape" = 1.0, "prior_rate" = 1.0)
+                chain_ctl <- list("n_iter" = n_iter)
+                mcmc_out <- .Call("beadListMcmcTest",
+                        processed_beads,
+                        chain_ctl,
+                        bead_prior, 
+                        PACKAGE = "rBalm")
+                return(mcmc_out)
+            }
+            n_dists <- 200
+            n <- rpois(n_dists, lambda = 200)
+            n_iter <- 1000
+            beads <- lapply(n, rnorm, mean = 0, sd = 1)
+            out <- beadListMcmcTest(beads, n_iter)
+            expect_equivalent(dim(out[[1]]), c(n_dists, n_iter))
+            expect_equivalent(dim(out[[1]]), c(n_dists, n_iter))
         })
 
